713f9a5d29e5686dbcf77725cce45e21ede3c5de
Adroso <adrian.lapico@my.jcu.edu.au>
2015-10-05 19:28:23 +1000

    Initial

---
+++ currency.py
@@ -0,0 +1,12 @@
+__author__ = 'Adroso'
+
+def convert(amount, home_currency_code, location_currency_code): # This function takes in input data from a home currency and converts it to another.
+
+    return
+
+
+def get_details(country_name): # This function intemperates data from a file
+
+    return
+
+
---
+++ trip.py
@@ -0,0 +1,20 @@
+__author__ = 'Adroso'
+
+class Error:
+    'Derived from the built-in Exception class, handles errors'
+
+
+
+class Country:
+    'Represents a single countrys details'
+
+
+class Details:
+    'Records sequence of countries visited during a trip'
+
+
+
+
+
+
+
