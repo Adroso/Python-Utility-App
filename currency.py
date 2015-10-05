6884473f805bc18dee998c0dee8bc54f00e98c64
Adroso <adrian.lapico@my.jcu.edu.au>
2015-10-05 22:49:00 +1000

    get details finished to a point, starting on convert

--- currency.py
+++ currency.py
@@ -1,17 +1,24 @@
 __author__ = 'Adroso'
 
-def convert(amount, home_currency_code, location_currency_code): # This function takes in input data from a home currency and converts it to another.
+import web_utility
 
+def convert(): # This function takes in input data from a home currency and converts it to another.
+
+    web_utility.load_page('https://www.google.com/finance/converter?a=1&from=AUD&to=JPY')
     return
 
-def get_details(): # This function interprets data from a file
+def get_details(country_name): # This function interprets data from a file
 
-    currency_details = open("currency_details.txt", "r")
-    print(file=currency_details)
-
+    currency_details = open("currency_details.txt", 'r') #load the currency details file as read
+    import re
+    for line in currency_details: #loops to each line
+        if country_name in line: #Searches for the input within the string (DOES NOT SEARCH FOR EXACT)
+            return print(line)
     currency_details.close()
-    return
+    return ();
 
-get_details()
 
+convert()
+country_name = input("Country")
+get_details(country_name)
 
