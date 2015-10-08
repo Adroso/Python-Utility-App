7df38cd56c514f18c9ab180c26c144b2a8bddcc3
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-10-08 17:38:51 +1000

    Update trip.py

--- trip.py
+++ trip.py
@@ -1,17 +1,47 @@
 __author__ = 'Adroso'
 
-class Error:
-    'Derived from the built-in Exception class, handles errors'
 
+
+class Error(Exception):
+    """Derived from the built-in Exception class, handles errors"""
+    def __init__(self, value):
+        super.__init__(value)
 
 
 class Country:
-    'Represents a single countrys details'
 
+    """Represents a single country's details"""
+    def __init__Country(self, name, currency_code, currency_symbol ):
+        self.name = name
+        self.currency_code = currency_code
+        self.currency_symbol = currency_symbol
+
+
+    def format_currency(value, currency_symbol):
+        return currency_symbol + format(value, ".2f")
+
+    def __str__(self, name, currency_code, currency_symbol ):
+        return name + currency_code + currency_symbol
 
 class Details:
-    'Records sequence of countries visited during a trip'
+    """Records sequence of countries visited during a trip"""
+    def __init__(self):
+        self.locations = []
 
+    def add(self, country_name, start_date, end_date):
+        import datetime
+
+        try:
+            datetime.datetime.strptime(start_date, '%Y-%m-%d')
+            datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        except:
+            raise Error("Incorrect data format, should be YYYY/MM/DD")
+
+        self.locations.append((country_name, start_date, end_date))
+
+
+    def is_empty(self):
+        return -1
 
 
 
