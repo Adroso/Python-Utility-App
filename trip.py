27a6b67c27e4f2dd338b508c4548f78118460a08
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-10-09 20:47:27 +1000

    Country() is fully completed

--- trip.py
+++ trip.py
@@ -1,27 +1,39 @@
 __author__ = 'Adroso'
 
+import datetime #Used for date input checks in Details()
 
 
-class Error(Exception):
+
+class Error(RuntimeError):
     """Derived from the built-in Exception class, handles errors"""
     def __init__(self, value):
+        self.value = value
         super.__init__(value)
+
+
+
+
 
 
 class Country:
 
     """Represents a single country's details"""
-    def __init__Country(self, name, currency_code, currency_symbol ):
+    def __init__(self, name, currency_code, currency_symbol ):
         self.name = name
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
 
 
-    def format_currency(value, currency_symbol):
-        return currency_symbol + format(value, ".2f")
+    def format_currency(self, value):
+        return self.currency_symbol + (format(value, ".0f"))
 
-    def __str__(self, name, currency_code, currency_symbol ):
-        return name + currency_code + currency_symbol
+    def __str__(self):
+        return str(self.name + ' ' + self.currency_code + ' ' + self.currency_symbol)
+
+
+
+
+
 
 class Details:
     """Records sequence of countries visited during a trip"""
@@ -29,22 +41,30 @@ class Details:
         self.locations = []
 
     def add(self, country_name, start_date, end_date):
-        import datetime
 
         try:
             datetime.datetime.strptime(start_date, '%Y-%m-%d')
             datetime.datetime.strptime(end_date, '%Y-%m-%d')
         except:
-            raise Error("Incorrect data format, should be YYYY/MM/DD")
+            raise Error("Incorrect format, should be YYYY/MM/DD")
+
+        try:
+            start_date > end_date
+        except:
+            print('The start date is after the end date')
 
         self.locations.append((country_name, start_date, end_date))
 
+    def current_country(self, date_string, country_name, start_date, end_date ):
+
+        if  start_date < date_string < end_date:
+            return country_name
+        else:
+            return 'lol'
 
     def is_empty(self):
-        return -1
-
-
-
-
-
+        if not self:
+            return 'Empty'
+        else:
+            return 'Not Empty'
 
