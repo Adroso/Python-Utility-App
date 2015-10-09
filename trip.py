56b41194e51960f1be1410c82f31a32f4663a047
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-10-10 01:10:52 +1000

    Update trip.py

--- trip.py
+++ trip.py
@@ -1,28 +1,22 @@
 __author__ = 'Adroso'
 
-import datetime #Used for date input checks in Details()
-
+import datetime  # Used for date input checks in Details()
 
 
 class Error(RuntimeError):
     """Derived from the built-in Exception class, handles errors"""
+
     def __init__(self, value):
-        self.value = value
         super.__init__(value)
 
 
-
-
-
-
 class Country:
-
     """Represents a single country's details"""
-    def __init__(self, name, currency_code, currency_symbol ):
+
+    def __init__(self, name, currency_code, currency_symbol):
         self.name = name
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
-
 
     def format_currency(self, value):
         return self.currency_symbol + (format(value, ".0f"))
@@ -31,40 +25,50 @@ class Country:
         return str(self.name + ' ' + self.currency_code + ' ' + self.currency_symbol)
 
 
-
-
-
-
 class Details:
     """Records sequence of countries visited during a trip"""
-    def __init__(self):
-        self.locations = []
 
-    def add(self, country_name, start_date, end_date):
+    def __init__(self, country_name, start_date, end_date):
+        self.locations = []
+        self.country_name = country_name
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def add(self, country_name, start_date, end_date ):
+        """Error Checking Section for add()"""
+
+        try:      # All parameters are text
+            isinstance(country_name, str)
+            isinstance(start_date, str)
+            isinstance(end_date, str)
+
+        except:
+            raise Error("Not Text")
 
         try:
-            datetime.datetime.strptime(start_date, '%Y-%m-%d')
-            datetime.datetime.strptime(end_date, '%Y-%m-%d')
+            datetime.datetime.strptime(self.start_date, '%Y-%m-%d')
+            datetime.datetime.strptime(self.end_date, '%Y-%m-%d')
         except:
             raise Error("Incorrect format, should be YYYY/MM/DD")
 
         try:
-            start_date > end_date
+            self.start_date > self.end_date
         except:
             print('The start date is after the end date')
 
-        self.locations.append((country_name, start_date, end_date))
+        self.locations.append((self.country_name, self.start_date, self.end_date))
 
-    def current_country(self, date_string, country_name, start_date, end_date ):
+    def current_country(self, date_string,):
 
-        if  start_date < date_string < end_date:
-            return country_name
+        if self.start_date < date_string < self.end_date:
+            return self.country_name
         else:
             return 'lol'
 
     def is_empty(self):
-        if not self:
-            return 'Empty'
-        else:
-            return 'Not Empty'
+        try:
+            if not self:
+                 return 'Empty'
+        except Error as Empty:
+            print(Empty)
 
