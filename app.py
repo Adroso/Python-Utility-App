59aca9598362e7d0582fa7a22b94a84bf72a263d
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-11-02 23:22:59 +1000

    Created initial build method

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+__author__ = 'Adroso'
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class currencyConverterApp(App):
+
+    def build(self):
+        Window.size = 350, 700
+        self.title = "Foreign Exchange Calculator"
+        self.root = Builder.load_file('gui.kv')
+        return self.root
+
+
+if __name__ == '__main__':
+    currencyConverterApp().run()
 
