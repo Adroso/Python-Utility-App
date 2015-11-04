e6509de03e8d811868ed46a9eca137df64838077
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-11-04 18:07:25 +1000

    Minor updates to currency

--- currency.py
+++ currency.py
@@ -51,6 +51,7 @@ def get_all_details():
     for line in details_file:
         split_details = line.split(",")
         all_country_details[split_details[0]] = (split_details[0], split_details[1], split_details[2])
+        details_file.close()
     return all_country_details
 
 
