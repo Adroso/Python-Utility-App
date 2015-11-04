6adb8a901cc0c4a53d6cd06e8fc21f8002420641
Adrian Lapico <adrian.lapico@my.jcu.edu.au>
2015-11-05 00:27:51 +1000

    Update currency.py

--- currency.py
+++ currency.py
@@ -36,8 +36,7 @@ def get_details(country_name):
         current_line = line.split(",")  # Splits data by commas to be exact matched in the following lines
         if country_name in line:
             if country_name == current_line[0]:  # Checks if the country name exactly matches the one in line.
-                line_details = current_line.strip('\n')
-                return line_details
+                return current_line
             else:
                 return ()
     currency_details.close()
@@ -52,92 +51,94 @@ def get_all_details():
     for line in details_file:
         split_details = line.split(",")
         all_country_details[split_details[0]] = (split_details[0], split_details[1], split_details[2])
-        details_file.close()
     return all_country_details
+
+
+
 
 
 """ Module Testing"""
 
-if __name__ == "__main__":
-    print('TESTING')
-    print('')
-    print('Test of convert()')
-    print("invalid conversion expect: -1", "1", "AUD", "->", "AUD", "=", convert(1, "AUD", "AUD"))
-    print("invalid conversion expect: -1", "1", "JPY", "->", "ABC", "=", convert(1, "JPY", "ABC"))
-    print("invalid conversion expect: -1", "1", "ABC", "->", "USD", "=", convert(1, "ABC", "USD"))
-    print("valid conversion", "10.95", "AUD", "->", "JPY", "=", convert(10.95, "AUD", "JPY"))
-    print("valid conversion reverse", "965.71", "JPY", "->", "AUD", "=", convert(965.71, "JPY", "AUD"))
-    print("valid conversion", "10.95", "AUD", "->", "BGN", "=", convert(10.95, "AUD", "BGN"))
-    print("valid conversion reverse", "13.82", "BGN", "->", "AUD", "=", convert(13.82, "BGN", "AUD"))
-    print("valid conversion", "200.15", "BGN", "->", "JPY", "=", convert(200.15, "BGN", "JPY"))
-    print("valid conversion reverse", "13390.51", "JPY", "->", "BGN", "=", convert(13390.51, "JPY", "BGN"))
-    print("valid conversion", "100", "JPY", "->", "USD", "=", convert(100, "JPY", "USD"))
-    print("valid conversion reverse", "0.83", "USD", "->", "JPY", "=", convert(0.83, "USD", "JPY"))
-    print("valid conversion", "19.99", "USD", "->", "BGN", "=", convert(19.99, "USD", "BGN"))
-    print("valid conversion reverse", "34.39", "BGN", "->", "USD", "=", convert(34.39, "BGN", "USD"))
-    print("valid conversion", "19.99", "USD", "->", "AUD", "=", convert(19.99, "USD", "AUD"))
-    print("valid conversion reverse", "27.26", "AUD", "->", "USD", "=", convert(27.26, "AUD", "USD"))
-    print('')
-    print('Testing get_details()')
+# if __name__ == "__main__":
+#     print('TESTING')
+#     print('')
+#     print('Test of convert()')
+#     print("invalid conversion expect: -1", "1", "AUD", "->", "AUD", "=", convert(1, "AUD", "AUD"))
+#     print("invalid conversion expect: -1", "1", "JPY", "->", "ABC", "=", convert(1, "JPY", "ABC"))
+#     print("invalid conversion expect: -1", "1", "ABC", "->", "USD", "=", convert(1, "ABC", "USD"))
+#     print("valid conversion", "10.95", "AUD", "->", "JPY", "=", convert(10.95, "AUD", "JPY"))
+#     print("valid conversion reverse", "965.71", "JPY", "->", "AUD", "=", convert(965.71, "JPY", "AUD"))
+#     print("valid conversion", "10.95", "AUD", "->", "BGN", "=", convert(10.95, "AUD", "BGN"))
+#     print("valid conversion reverse", "13.82", "BGN", "->", "AUD", "=", convert(13.82, "BGN", "AUD"))
+#     print("valid conversion", "200.15", "BGN", "->", "JPY", "=", convert(200.15, "BGN", "JPY"))
+#     print("valid conversion reverse", "13390.51", "JPY", "->", "BGN", "=", convert(13390.51, "JPY", "BGN"))
+#     print("valid conversion", "100", "JPY", "->", "USD", "=", convert(100, "JPY", "USD"))
+#     print("valid conversion reverse", "0.83", "USD", "->", "JPY", "=", convert(0.83, "USD", "JPY"))
+#     print("valid conversion", "19.99", "USD", "->", "BGN", "=", convert(19.99, "USD", "BGN"))
+#     print("valid conversion reverse", "34.39", "BGN", "->", "USD", "=", convert(34.39, "BGN", "USD"))
+#     print("valid conversion", "19.99", "USD", "->", "AUD", "=", convert(19.99, "USD", "AUD"))
+#     print("valid conversion reverse", "27.26", "AUD", "->", "USD", "=", convert(27.26, "AUD", "USD"))
+#     print('')
+#     print('Testing get_details()')
+# 
+#     print("invalid details expect ():", get_details("Unknown"))
+#     print("invalid details expect ():", get_details("Japanese"))
+#     print("invalid details expect ():", get_details(""))
+#     print("valid details expect details of AUS:", get_details("Australia"))
+#     print("valid details expect details of JPN:", get_details("Japan"))
+#     print("valid details expect details of HK:", get_details("Hong Kong"))
 
-    print("invalid details expect ():", get_details("Unknown"))
-    print("invalid details expect ():", get_details("Japanese"))
-    print("invalid details expect ():", get_details(""))
-    print("valid details expect details of AUS:", get_details("Australia"))
-    print("valid details expect details of JPN:", get_details("Japan"))
-    print("valid details expect details of HK:", get_details("Hong Kong"))
-
-    # New get_all_details testing:
-    print(get_all_details())
-    testing = get_all_details()
-    print("keys", testing.keys())
-    print(testing["Australia"])
+# New get_all_details testing:
+print(get_all_details())
+testing = get_all_details()
+print("keys", testing.keys())
+print(testing["Australia"])
 
 
-    # Manual Testing
-    # converted_save = 0
-    #
-    # loop_check = True
-    # while loop_check:
-    #
-    #     amount = input('Amount to change:') # Assume value will be a number
-    #     home = str(input('Home Currency Code:'))
-    #     away = str(input('Away Currency Code:'))
-    #
-    #     converted = convert(amount, home, away)
-    #
-    #     if converted == -1:
-    #         check = 'invalid conversion'
-    #     elif converted_save == amount:
-    #         check = 'valid conversion reverse'
-    #     else:
-    #         check = 'valid conversion'
-    #
-    #     converted_save = converted
-    #
-    #     print(check, ' ', amount, ' ', home, '->', away, ' ', converted)
-    #
-    #     loop_check = input('check again or move on to next check? Y or N').upper()
-    #     if loop_check == 'Y':
-    #         loop_check = True
-    #     else:
-    #         loop_check = False
-    #
-    # loop_check = True
-    # while loop_check:
-    #     country_name = input("Input Test Country:").title()
-    #     details = get_details(country_name)
-    #
-    #     if not details:
-    #         check = 'invalid details'
-    #     else:
-    #         check = 'valid details'
-    #
-    #     print(check, ' ', country_name, ' ', details)
-    #
-    #     loop_check = input('check again or finnish? Y or N').upper()
-    #     if loop_check == 'Y':
-    #         loop_check = True
-    #     else:
-    #         loop_check = False
-    #         print('Thank You :)')
+# Manual Testing
+# converted_save = 0
+#
+# loop_check = True
+# while loop_check:
+#
+#     amount = input('Amount to change:') # Assume value will be a number
+#     home = str(input('Home Currency Code:'))
+#     away = str(input('Away Currency Code:'))
+#
+#     converted = convert(amount, home, away)
+#
+#     if converted == -1:
+#         check = 'invalid conversion'
+#     elif converted_save == amount:
+#         check = 'valid conversion reverse'
+#     else:
+#         check = 'valid conversion'
+#
+#     converted_save = converted
+#
+#     print(check, ' ', amount, ' ', home, '->', away, ' ', converted)
+#
+#     loop_check = input('check again or move on to next check? Y or N').upper()
+#     if loop_check == 'Y':
+#         loop_check = True
+#     else:
+#         loop_check = False
+#
+# loop_check = True
+# while loop_check:
+#     country_name = input("Input Test Country:").title()
+#     details = get_details(country_name)
+#
+#     if not details:
+#         check = 'invalid details'
+#     else:
+#         check = 'valid details'
+#
+#     print(check, ' ', country_name, ' ', details)
+#
+#     loop_check = input('check again or finnish? Y or N').upper()
+#     if loop_check == 'Y':
+#         loop_check = True
+#     else:
+#         loop_check = False
+#         print('Thank You :)')
